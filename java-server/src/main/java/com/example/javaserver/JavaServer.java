ea603c290f8c9b0091b2c5bd692b835dca27e5fa
Shypulll <danikshypul@gmail.com>
2025-03-20 22:29:03 +0100

    Добавил новую фичу

---
+++ java-server/src/main/java/com/example/javaserver/JavaServer.java
@@ -0,0 +1,26 @@
+package com.example.javaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class JavaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaServer.class, args);
+    }
+
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
+    }
+}
